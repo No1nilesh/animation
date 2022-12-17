7bc0e2c693b87a58676dff89538a69897d9aa6f8
Nilesh Gautam <gautamnilesh084@gmail.com>
2022-12-17 12:58:50 +0530

    innitial commit

---
+++ JavaScript/navfunc.js
@@ -0,0 +1,19 @@
+const hamburgerEl = document.querySelector("#hamburger");
+const sidebarEl = document.querySelector("#sidebar");
+
+
+document.onclick = function(e){
+    console.log(e.target.id)
+    if(e.target.id !== 'sidebar' && e.target.id !== 'hamburger'  ){
+            sidebarEl.classList.remove("open")
+            hamburgerEl.classList.remove("open")
+    
+    }
+}
+
+hamburgerEl.onclick =function(){
+
+    sidebarEl.classList.toggle("open")
+  hamburgerEl.classList.toggle("open")
+ 
+}
